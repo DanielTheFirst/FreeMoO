fa21024ae9a806d4e320ea159451f59c37a091c4
Daniel Reslie <daniel.reslie@gmail.com>
2016-03-18 23:03:57 -0400

    Refactored the timer some more.  The VS refactor tool is cool too.

---
+++ FreemoodSDL/Timer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FreeMoO
+{
+    public class Timer
+    {
+        private DateTime _currentTime;
+        private TimeSpan _sinceLastFrame;
+        private DateTime _startTime;
+        private TimeSpan _sinceBeginning;
+
+        public Timer()
+        {
+            _startTime = DateTime.Now;
+            _sinceBeginning = DateTime.Now.Subtract(_startTime);
+            _currentTime = _startTime;
+            _sinceLastFrame = _sinceBeginning;
+        }
+
+        public void Update()
+        {
+            DateTime tmpNOw = DateTime.Now;
+            _sinceLastFrame = tmpNOw.Subtract(_currentTime);
+            _sinceBeginning = tmpNOw.Subtract(_startTime);
+            _currentTime = tmpNOw;
+        }
+
+        public double MillisecondsElapsed
+        {
+            get
+            {
+                return _sinceLastFrame.TotalMilliseconds;
+            }
+        }
+
+        public double TotalMilliseconds
+        {
+            get
+            {
+                return _sinceBeginning.TotalMilliseconds;
+            }
+        }
+
+        public double SecondsElapsed
+        {
+            get
+            {
+                return _sinceLastFrame.TotalSeconds;
+            }
+        }
+
+        public double TotalSeconds
+        {
+            get
+            {
+                return _sinceBeginning.TotalSeconds;
+            }
+        }
+    }
+}
